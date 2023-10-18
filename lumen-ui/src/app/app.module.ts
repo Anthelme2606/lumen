d8a0bd56198be4d15ca289598a8916fd89ce987c
Tountoun <tountounabela@gmail.com>
2023-10-18 13:59:49 +0000

    Add navigation bar

---
+++ lumen-ui/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
